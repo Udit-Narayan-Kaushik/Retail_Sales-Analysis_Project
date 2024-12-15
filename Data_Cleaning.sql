a6b0724f42e1d9abfcad33529c3c8e890bf754ce
Udit_Narayan_Kaushik <155947564+Udit-Narayan-Kaushik@users.noreply.github.com>
2024-12-15 19:29:41 +0530

    Add files via upload

---
+++ Data_Cleaning.sql
@@ -0,0 +1,53 @@
+
+
+
+SELECT 
+    COUNT(*) 
+FROM retail_sales
+
+-- Data Cleaning
+SELECT * FROM retail_sales
+WHERE transactions_id IS NULL
+
+SELECT * FROM retail_sales
+WHERE sale_date IS NULL
+
+SELECT * FROM retail_sales
+WHERE sale_time IS NULL
+
+SELECT * FROM retail_sales
+WHERE 
+    transaction_id IS NULL
+    OR
+    sale_date IS NULL
+    OR 
+    sale_time IS NULL
+    OR
+    gender IS NULL
+    OR
+    category IS NULL
+    OR
+    quantity IS NULL
+    OR
+    cogs IS NULL
+    OR
+    total_sale IS NULL;
+    
+-- 
+DELETE FROM retail_sales
+WHERE 
+    transaction_id IS NULL
+    OR
+    sale_date IS NULL
+    OR 
+    sale_time IS NULL
+    OR
+    gender IS NULL
+    OR
+    category IS NULL
+    OR
+    quantity IS NULL
+    OR
+    cogs IS NULL
+    OR
+    total_sale IS NULL;
---
+++ Data_Exploration.sql
@@ -0,0 +1,12 @@
+-- Data Exploration
+
+-- How many sales we have?
+SELECT COUNT(*) as total_sale FROM retail_sales
+
+-- How many uniuque customers we have ?
+
+SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales
+
+-- How many Business category do we have
+
+SELECT DISTINCT category FROM retail_sales
